0616dccd1f52d9ecbbf5de10e5974426b0a4d149
azarakhshzi <zi.azarakhsh@gmail.com>
2025-07-14 14:03:22 +0200

    edit

--- models/prep/prep_weather_hourly.sql
+++ models/prep/prep_weather_hourly.sql
@@ -1,32 +1,30 @@
 WITH hourly_data AS (
     SELECT * 
-    FROM {{ ref('staging_weather_hourly') }}
+    FROM {{ref('staging_weather_hourly')}}
 ),
-
 add_features AS (
     SELECT *
-        , timestamp::DATE AS date                            -- only date
-        , timestamp::TIME AS time                            -- only time
-        , TO_CHAR(timestamp, 'HH24:MI') AS hour              -- hours:minutes as text
-        , TO_CHAR(timestamp, 'FMMonth') AS month_name        -- month name as text
-        , TO_CHAR(timestamp, 'Day') AS weekday               -- weekday name as text
+		, timestamp::DATE AS date               -- only date (hours:minutes:seconds) as DATE data type
+		, timestamp::TIME AS time                           -- only time (hours:minutes:seconds) as TIME data type
+        , TO_CHAR(timestamp,'HH24:MI') as hour  -- time (hours:minutes) as TEXT data type
+        , TO_CHAR(timestamp, 'FMmonth') AS month_name   -- month name as a TEXT
+        , TO_CHAR(timestamp, 'Day') AS weekday        -- weekday name as TEXT        
         , DATE_PART('day', timestamp) AS date_day
-        , DATE_PART('month', timestamp) AS date_month
-        , DATE_PART('year', timestamp) AS date_year
-        , DATE_PART('week', timestamp) AS cw
+		, DATE_PART('month', timestamp) AS date_month
+		, DATE_PART('year', timestamp) AS date_year
+		, DATE_PART('week', timestamp) AS cw
     FROM hourly_data
 ),
-
 add_more_features AS (
     SELECT *
-        , CASE 
-            WHEN time BETWEEN TIME '00:00:00' AND TIME '05:59:59' THEN 'night'
-            WHEN time BETWEEN TIME '06:00:00' AND TIME '17:59:59' THEN 'day'
-            WHEN time BETWEEN TIME '18:00:00' AND TIME '23:59:59' THEN 'evening'
-          END AS day_part
+		,(CASE 
+	    WHEN time BETWEEN TIME '22:00:00' AND TIME '23:59:59' THEN 'night'
+	    WHEN time BETWEEN TIME '00:00:00' AND TIME '04:59:59' THEN 'night'
+	    WHEN time BETWEEN TIME '05:00:00' AND TIME '11:00:00' THEN 'day'
+	    WHEN time BETWEEN TIME '11:01:00' AND TIME '21:59:59' THEN 'evening'
+		END) AS day_part
     FROM add_features
 )
 
 SELECT *
 FROM add_more_features
-ORDER BY timestamp
