211dfb6073a499890d66d3b2e5cf7d3691ec1aba
azarakhshzi <zi.azarakhsh@gmail.com>
2025-07-14 14:10:23 +0200

    edit

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -4,22 +4,22 @@ WITH daily_data AS (
 ),
 add_features AS (
     SELECT *
-		, DATE_PART('day', date) AS date_day 		-- number of the day of month
-		, DATE_PART('month', date) AS date_month 	-- number of the month of year
-		, DATE_PART('year', date) AS date_year 		-- number of year
-		, DATE_PART('week', date) AS cw 			-- number of the week of year
-		, TRIM(TO_CHAR(date, 'Month')) AS month_name 	-- name of the month
-		, TO_CHAR(date, 'Day') AS weekday 		-- name of the weekday
+        , DATE_PART('day', date) AS date_day              -- number of the day of month
+        , DATE_PART('month', date) AS date_month          -- number of the month of year
+        , DATE_PART('year', date) AS date_year            -- number of year
+        , DATE_PART('week', date) AS cw                   -- number of the week of year
+        , TO_CHAR(date, 'Month') AS month_name            -- name of the month
+        , TO_CHAR(date, 'Day') AS weekday                 -- name of the weekday
     FROM daily_data 
 ),
 add_more_features AS (
     SELECT *
-		, (CASE 
-			WHEN month_name in ('December', 'January', 'February') THEN 'winter'
-			WHEN month_name in ('March', 'April', 'May') THEN 'spring'
-            WHEN month_name in ('June', 'July', 'August') THEN 'summer'
-            WHEN month_name in ('September', 'October', 'November') THEN 'autumn'
-		END) AS season
+        , CASE 
+            WHEN date_month IN (12, 1, 2) THEN 'winter'
+            WHEN date_month IN (3, 4, 5) THEN 'spring'
+            WHEN date_month IN (6, 7, 8) THEN 'summer'
+            WHEN date_month IN (9, 10, 11) THEN 'autumn'
+          END AS season
     FROM add_features
 )
 SELECT *
