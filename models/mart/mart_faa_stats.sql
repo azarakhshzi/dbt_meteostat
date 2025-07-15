dfdf1d49ad234510b0bf22767b091d8f6ed1376a
azarakhshzi <zi.azarakhsh@gmail.com>
2025-07-15 12:51:00 +0200

    add faa anf rout

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -1,72 +1,50 @@
-with flights_cleaned AS ( 
-	SELECT * 
-	FROM {{ref('prep_flights')}}
+WITH departures AS ( 
+	SELECT origin AS faa
+			,COUNT(DISTINCT origin) AS nunique_to
+			,COUNT(sched_dep_time) AS dep_planned
+			,SUM(cancelled) AS dep_cancelled
+			,SUM(diverted) AS dep_diverted
+			,COUNT(dep_time) AS dep_n_flights
+			,COUNT(DISTINCT tail_number) AS dep_nunique_tails -- BONUS TASK
+			,COUNT(DISTINCT airline) AS dep_nunique_airlines -- BONUS TASK
+	FROM {{ref('prep_flights')}} 
+	GROUP BY origin
+	ORDER BY origin
 ),
-unique_departures as (
-	SELECT COUNT(DISTINCT origin) AS num_unique_departure --unique number of departures connections
-	FROM flights_cleaned),
-unique_arrivals AS ( 
-	SELECT COUNT(DISTINCT dest) AS num_unique_arrival --unique number of arrival connections
-	FROM flights_cleaned),
-total_flight AS (
-	SELECT num_unique_departure + num_unique_arrival AS total_flights -- ow many flight were planned in total (departures & arrivals)
-	FROM unique_departures,unique_arrivals ),
-canceled_flights AS ( 
-	SELECT count(*)  AS total_cancelled_flights -- how many flights were canceled in total (departures & arrivals)
-	FROM flights_cleaned 
-	WHERE cancelled = 1 
-	),
-diverted_flights AS ( 
-	SELECT count(*)  AS total_diverted_flights  --how many flights were diverted in total (departures & arrivals)
-	FROM flights_cleaned 
-	WHERE diverted = 1 
-	),
-occurred_flights AS ( 
-	SELECT count(*)  AS total_occurred_flights -- how many flights actually occured in total (departures & arrivals)
-	FROM flights_cleaned 
-	WHERE cancelled = 0 AND diverted = 0 
-	),
-avg_unique_airplanes_travelled AS ( 
-	SELECT ROUND(AVG(daily_unique_airplane), 2) AS avg_daily_unique_airplane_travelled -- how many unique airplanes travelled on average
-	FROM (
-		SELECT 
-		flight_date, 
-		count (DISTINCT tail_number) AS daily_unique_airplane
-		FROM flights_cleaned 
-		WHERE tail_number IS NOT NULL
-		GROUP BY flight_date
-		) subquery
-	),
-avg_unique_airplanes_serviced AS ( 
-	SELECT ROUND(AVG(daily_unique_airplane), 2) AS avg_daily_unique_airplane_serviced -- how many unique airlines were in service on average
-	FROM (
-		SELECT 
-		flight_date, 
-		count (DISTINCT tail_number) AS daily_unique_airplane
-		FROM flights_cleaned
-		WHERE cancelled = 0
-		GROUP BY flight_date
-		) subquery
-		),
-	airport_cleaned AS (
-		SELECT faa, name, city, country -- -- add city, country and name of the airport
-		FROM {{ref('prep_airports')}}
-    )
-	SELECT 
-		num_unique_departure,
-		num_unique_arrival,
-		total_flights,
-		total_cancelled_flights,
-		total_diverted_flights,
-		total_occurred_flights,
-		avg_daily_unique_airplane_travelled,
-		avg_daily_unique_airplane_serviced,
-	
-	FROM unique_departures
-	CROSS JOIN unique_arrivals
-	CROSS JOIN total_flight
-	CROSS JOIN canceled_flights
-	CROSS JOIN diverted_flights
-	CROSS JOIN occurred_flights
-	CROSS JOIN avg_unique_airplanes_travelled
-	CROSS JOIN avg_unique_airplanes_serviced
+arrivals AS (
+	SELECT dest AS faa
+			,COUNT(DISTINCT dest) AS nunique_from
+			,COUNT(sched_arr_time) AS arr_planned
+			,SUM(cancelled) AS arr_cancelled
+			,SUM(diverted) AS arr_diverted
+			,COUNT(arr_time) AS arr_n_flights
+			,COUNT(DISTINCT tail_number) AS arr_nunique_tails -- BONUS TASK
+			,COUNT(DISTINCT airline) AS arr_nunique_airlines -- BONUS TASK
+	FROM {{ref('prep_flights')}} 
+	GROUP BY dest
+	ORDER BY dest
+),
+total_stats AS (
+	SELECT faa
+			,nunique_to
+			,nunique_from
+			,(nunique_to + nunique_from)::NUMERIC/2 AS n_connections -- fractions would indicate that for the number of connections to!=from 
+			,dep_planned + arr_planned AS total_planned
+			,dep_cancelled + arr_cancelled AS total_cancelled
+			,dep_diverted + arr_diverted AS total_diverted
+			,((dep_cancelled + arr_cancelled + dep_diverted + arr_diverted)::NUMERIC/(dep_planned + arr_planned)::NUMERIC)*100 AS percent_change -- BONUS TASK
+			,dep_n_flights + arr_n_flights AS total_flights
+			,(dep_nunique_tails + arr_nunique_tails)::NUMERIC/2 AS nunique_tails -- fractions would indicate that for the number of tails to!=from -- BONUS TASK
+			,(dep_nunique_airlines + arr_nunique_airlines)::NUMERIC/2 AS nunique_airlines -- fractions would indicate that for the number of airlines to!=from -- BONUS TASK
+	FROM departures
+	JOIN arrivals
+	USING (faa)
+)
+SELECT a.city
+		,a.country
+		,a.name
+		, t.* 
+FROM total_stats t
+LEFT JOIN {{ref('prep_airports')}} a
+USING (faa)
+ORDER BY total_diverted DESC
